00dab8791a3ba86c5b93ee1de44d5c5c1c364ff5
Lex Tang <lexrus@gmail.com>
2020-07-12 01:50:10 +0800

    format JSON with JavaScriptCore

--- XcodeExtension/FormatJSONCommand.swift
+++ XcodeExtension/FormatJSONCommand.swift
@@ -8,39 +8,83 @@
 
 import Foundation
 import XcodeKit
+import JavaScriptCore
 
 enum FormatJSONError : Error {
     case bufferError
     case invalidJSON
     case invalidData
+    case jsContextFailure
+    case failedToFormat
+}
+
+enum FormatEngine {
+    case jsonSerializer
+    case javaScriptCore
 }
 
 class FormatJSONCommand: NSObject, XCSourceEditorCommand {
+
+    // TODO: Provide as an option
+    private var formatEngine: FormatEngine = .javaScriptCore
     
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
         let lines = invocation.buffer.lines
 
-        do {
-            // FIXME: We should format the completeBuffer when nothing selected. Otherwise try format selection first.
-            guard let data = invocation.buffer.completeBuffer.data(using: .utf8, allowLossyConversion: true) else {
+        // FIXME: We should format the completeBuffer when nothing selected. Otherwise try format selection first.
+        let sourceString = invocation.buffer.completeBuffer
+
+        let formattedJSONString: String
+
+        switch formatEngine {
+
+        // MARK: - JavaScriptCore
+        case .javaScriptCore:
+            guard let jsContext = JSContext() else {
+                completionHandler(FormatJSONError.jsContextFailure)
+                return
+            }
+
+            jsContext.setObject(sourceString, forKeyedSubscript: "jsonString" as NSString)
+
+            let indentation = invocation.buffer.usesTabsForIndentation
+                ? "\t"
+                : [String](repeating: " ", count: invocation.buffer.indentationWidth).joined()
+
+            let js = "try{JSON.stringify(JSON.parse(jsonString), null, '\(indentation)')}catch{}"
+
+            guard let result = jsContext.evaluateScript(js), result.isString else {
+                completionHandler(FormatJSONError.failedToFormat)
+                return
+            }
+
+            formattedJSONString = result.toString()
+
+        // MARK: - JSONSerializer
+        case .jsonSerializer:
+            guard let data = sourceString.data(using: .utf8, allowLossyConversion: true) else {
                 completionHandler(FormatJSONError.bufferError)
                 return
             }
 
-            let json = try JSONSerialization.jsonObject(with: data, options: [.allowFragments, .mutableLeaves])
-            let formattedData = try JSONSerialization.data(withJSONObject: json, options: [.fragmentsAllowed, .prettyPrinted])
-            guard let formattedString = String(data: formattedData, encoding: .utf8) else {
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [.allowFragments, .mutableLeaves])
+                let formattedData = try JSONSerialization.data(withJSONObject: json, options: [.fragmentsAllowed, .prettyPrinted])
+                guard let result = String(data: formattedData, encoding: .utf8) else {
+                    completionHandler(FormatJSONError.invalidData)
+                    return
+                }
+
+                formattedJSONString = result
+            } catch {
                 completionHandler(FormatJSONError.invalidData)
                 return
             }
-
-            lines.removeAllObjects()
-            lines.addObjects(from: [formattedString])
-
-            completionHandler(nil)
-        } catch {
-            completionHandler(error)
         }
+
+        lines.removeAllObjects()
+        lines.addObjects(from: [formattedJSONString])
+        completionHandler(nil)
     }
     
 }
