576a0fe2c67130865488b8671fe0dd91f8b5fc84
Lex Tang <lexrus@gmail.com>
2020-07-11 23:08:55 +0800

    Initial Commit

---
+++ iDevBox/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  iDevBox
+//
+//  Created by Lex on 2020/7/11.
+//  Copyright © 2020 Lex. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
